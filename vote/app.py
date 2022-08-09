255d3830682da21b588e140f8e7924b258f60304
Bueller <bueller@gmail.com>
2022-08-09 12:51:25 -0400

    Demo for han

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Hans")
+option_b = os.getenv('OPTION_B', "Solos")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
