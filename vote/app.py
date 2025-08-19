313d4414e7e5fafd2ea9f8e907cbbe1ab73b6c3c
Bueller <bueller@gmail.com>
2025-08-19 16:36:04 -0400

    Update app.py trying to break

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Siamese")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_b = os('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
